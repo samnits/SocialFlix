35cdae39e6b00327774ae1d38f843264adac3aec
samnits <sakshammaurya18@gmail.com>
2025-07-29 12:33:38 +0530

    update

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+
 
 import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action';
 import { notFound } from 'next/navigation';
